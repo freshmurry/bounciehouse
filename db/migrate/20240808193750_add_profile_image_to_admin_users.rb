cbfc935a8bfcfbbb927d7c27d2454ca5aaf50d3b
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-12 19:02:04 -0500

    Trying to fix admin_user

--- db/migrate/20240808193750_add_profile_image_to_admin_users.rb
+++ db/migrate/20240808193750_add_profile_image_to_admin_users.rb
@@ -1,22 +1,5 @@
 class AddProfileImageToAdminUsers < ActiveRecord::Migration[5.0]
   def change
     add_attachment :admin_users, :profile_image
-
-    # Only add columns if they do not already exist
-    unless column_exists?(:admin_users, :profile_image_file_name)
-      add_column :admin_users, :profile_image_file_name, :string
-    end
-
-    unless column_exists?(:admin_users, :profile_image_content_type)
-      add_column :admin_users, :profile_image_content_type, :string
-    end
-
-    unless column_exists?(:admin_users, :profile_image_file_size)
-      add_column :admin_users, :profile_image_file_size, :integer
-    end
-
-    unless column_exists?(:admin_users, :profile_image_updated_at)
-      add_column :admin_users, :profile_image_updated_at, :datetime
-    end
   end
 end
