81f7d8ac9a1035795115ee64b68392d1e010807b
Lawrence Murry <lawrencemurry@yahoo.com>
2025-04-24 02:14:23 -0500

    Update review.rb

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,24 +1,21 @@
 ActiveAdmin.register Review do
-  permit_params :comment, :star, :bouncehouse_id, :reservation_id, :guest_id, :host_id, :type
+  permit_params :comment, :star, :bouncehouse_id, :guest_id, :host_id, :created_at
 
   index do
     selectable_column
     id_column
     column :comment
     column :star
-    column :bouncehouse
-    column :reservation
-    column :guest
-    column :host
-    column :type
-    column :created_at
+    column("Bouncehouse ID") { |review| review.bouncehouse_id }
+    column("Guest") { |review| review.guest&.fullname }
+    column("Host") { |review| review.host&.fullname }
+    column("Reservation Date", :created_at)
     actions
   end
 
-  filter :bouncehouse
-  filter :reservation
-  filter :guest
-  filter :host
+  filter :bouncehouse_id
+  filter :guest, collection: proc { User.all.map { |u| [u.fullname, u.id] } }
+  filter :host, collection: proc { User.all.map { |u| [u.fullname, u.id] } }
   filter :star
   filter :created_at
 
@@ -26,11 +23,10 @@ ActiveAdmin.register Review do
     f.inputs do
       f.input :comment
       f.input :star
-      f.input :bouncehouse
-      f.input :reservation
-      f.input :guest
-      f.input :host
-      f.input :type
+      f.input :bouncehouse_id, label: "Bouncehouse ID"
+      f.input :guest, label: "Guest", collection: User.all.map { |u| [u.fullname, u.id] }
+      f.input :host, label: "Host", collection: User.all.map { |u| [u.fullname, u.id] }
+      f.input :created_at, as: :datepicker, label: "Reservation Date"
     end
     f.actions
   end
@@ -39,12 +35,10 @@ ActiveAdmin.register Review do
     attributes_table do
       row :comment
       row :star
-      row :bouncehouse
-      row :reservation
-      row :guest
-      row :host
-      row :type
-      row :created_at
+      row("Bouncehouse ID") { |review| review.bouncehouse_id }
+      row("Guest") { |review| review.guest&.fullname }
+      row("Host") { |review| review.host&.fullname }
+      row("Reservation Date") { |review| review.created_at }
       row :updated_at
     end
   end
