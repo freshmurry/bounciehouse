4b016278e7a411fb5d89b219e342805cd16beef6
Lawrence <lawrencemurry@yahoo.com>
2025-06-22 16:47:21 -0500

    Update Ruby version, gems, and schema after successful migration

--- db/schema.rb
+++ db/schema.rb
@@ -2,51 +2,6 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-<<<<<<< HEAD
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20240808193750) do
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_type"
-    t.integer  "resource_id"
-    t.string   "author_type"
-    t.integer  "author_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                      default: "", null: false
-    t.string   "name",                       default: "", null: false
-    t.string   "description",                default: "", null: false
-    t.string   "encrypted_password",         default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "profile_image_file_name"
-    t.string   "profile_image_content_type"
-    t.bigint   "profile_image_file_size"
-    t.datetime "profile_image_updated_at"
-=======
 # This file is the source Rails uses to define your schema when running `bin/rails
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
@@ -114,37 +69,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "profile_image"
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "bouncehouses", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "bouncehouse_type"
-    t.string   "address"
-    t.string   "listing_name"
-    t.text     "description"
-    t.string   "time_limit"
-    t.integer  "price"
-    t.integer  "tip"
-    t.string   "pickup_type"
-    t.boolean  "is_heated"
-    t.boolean  "is_slide"
-    t.boolean  "is_waterslide"
-    t.boolean  "is_basketball_hoop"
-    t.boolean  "is_lighting"
-    t.boolean  "is_speakers"
-    t.boolean  "is_wall_climb"
-    t.boolean  "is_sprinkler"
-    t.boolean  "active"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.integer  "instant"
-    t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-=======
     t.string "bouncehouse_type"
     t.string "address"
     t.string "listing_name"
@@ -168,44 +97,20 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
     t.integer "user_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["user_id"], name: "index_bouncehouses_on_user_id"
   end
 
   create_table "calendars", force: :cascade do |t|
-<<<<<<< HEAD
-    t.date     "day"
-    t.integer  "price"
-    t.integer  "status"
-    t.integer  "bouncehouse_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-=======
     t.date "day"
     t.integer "price"
     t.integer "status"
     t.integer "bouncehouse_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["bouncehouse_id"], name: "index_calendars_on_bouncehouse_id"
   end
 
   create_table "conversations", force: :cascade do |t|
-<<<<<<< HEAD
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "context"
-    t.integer  "user_id"
-    t.integer  "conversation_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-=======
     t.integer "sender_id"
     t.integer "recipient_id"
     t.datetime "created_at", precision: nil, null: false
@@ -227,50 +132,19 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
     t.integer "conversation_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-=======
     t.string "content"
     t.integer "user_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
-<<<<<<< HEAD
-    t.integer  "bouncehouse_id",     null: false
-    t.string   "image"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.bigint   "image_file_size"
-    t.datetime "image_updated_at"
-    t.index ["bouncehouse_id"], name: "index_photos_on_bouncehouse_id"
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "bouncehouse_id"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.integer  "price"
-    t.integer  "total"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "status",         default: 0
-=======
     t.integer "bouncehouse_id", null: false
     t.string "image"
     t.datetime "created_at", precision: nil, null: false
@@ -290,23 +164,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.integer "status", default: 0
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["bouncehouse_id"], name: "index_reservations_on_bouncehouse_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
-<<<<<<< HEAD
-    t.text     "comment"
-    t.integer  "star",           default: 1
-    t.integer  "bouncehouse_id"
-    t.integer  "reservation_id",             null: false
-    t.integer  "guest_id",                   null: false
-    t.integer  "host_id",                    null: false
-    t.string   "type"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-=======
     t.text "comment"
     t.integer "star", default: 1
     t.integer "bouncehouse_id"
@@ -316,7 +178,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
     t.string "type"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["bouncehouse_id"], name: "index_reviews_on_bouncehouse_id"
     t.index ["guest_id"], name: "index_reviews_on_guest_id"
     t.index ["host_id"], name: "index_reviews_on_host_id"
@@ -324,62 +185,15 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
   end
 
   create_table "settings", force: :cascade do |t|
-<<<<<<< HEAD
-    t.boolean  "enable_sms",   default: true
-    t.boolean  "enable_email", default: true
-    t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-=======
     t.boolean "enable_sms", default: true
     t.boolean "enable_email", default: true
     t.integer "user_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
     t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "fullname"
-    t.string   "phone_number"
-    t.string   "address"
-    t.text     "description"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
-    t.string   "access_token"
-    t.string   "pin"
-    t.boolean  "phone_verified"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "stripe_id"
-    t.string   "merchant_id"
-    t.integer  "unread",                 default: 0
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.bigint   "image_file_size"
-    t.datetime "image_updated_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-=======
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -436,5 +250,4 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_21_232335) do
   add_foreign_key "reviews", "users", column: "guest_id"
   add_foreign_key "reviews", "users", column: "host_id"
   add_foreign_key "settings", "users"
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
 end
