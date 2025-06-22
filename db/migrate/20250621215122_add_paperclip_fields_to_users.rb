68a3673a7449c2b957232ae14cb6206847e94739
root <root@lmurry.localdomain>
2025-06-22 15:46:49 -0500

    Fix: only add paperclip fields if they do not exist

--- db/migrate/20250621215122_add_paperclip_fields_to_users.rb
+++ db/migrate/20250621215122_add_paperclip_fields_to_users.rb
@@ -1,8 +1,8 @@
 class AddPaperclipFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :image_file_name, :string
-    add_column :users, :image_content_type, :string
-    add_column :users, :image_file_size, :integer
-    add_column :users, :image_updated_at, :datetime
+    add_column :users, :image_file_name, :string unless column_exists?(:users, :image_file_name)
+    add_column :users, :image_content_type, :string unless column_exists?(:users, :image_content_type)
+    add_column :users, :image_file_size, :integer unless column_exists?(:users, :image_file_size)
+    add_column :users, :image_updated_at, :datetime unless column_exists?(:users, :image_updated_at)
   end
 end
