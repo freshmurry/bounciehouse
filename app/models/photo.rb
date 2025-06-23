6f18f0e0d1c7b80609bb4e3554a9695f859a2138
Lawrence <lawrencemurry@yahoo.com>
2025-06-23 16:27:42 -0500

    Trying to get images to render in production

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,18 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :bouncehouse
-  has_many_attached :images
-
-  # Validation is not necessary for ActiveStorage since it validates automatically for content types
-  # If you need to validate the image type, you can do so with a custom validation:
-  validate :correct_image_type
-
-  private
-
-  def correct_image_type
-    images.each do |image|
-      unless image.content_type.starts_with?('image/')
-        errors.add(:images, 'must be an image')
-      end
-    end
-  end
+  
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
