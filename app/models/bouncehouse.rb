6b007abfde8144cee21cf1d50ce4df67d2d0b19d
Lawrence <lawrencemurry@yahoo.com>
2025-06-23 08:33:47 -0500

    Trying to get images to render in production

--- app/models/bouncehouse.rb
+++ app/models/bouncehouse.rb
@@ -2,7 +2,9 @@ class Bouncehouse < ApplicationRecord
   enum instant: { Request: 1, Instant: 0 }
 
   belongs_to :user
-  has_many_attached :photos
+
+  # Use the Photo model association, not direct Active Storage
+  has_many :photos, dependent: :destroy
 
   has_many :reservations
   has_many :guest_reviews
@@ -21,10 +23,12 @@ class Bouncehouse < ApplicationRecord
     end
   end
 
+  # Use the first photo's image as the cover photo
   def cover_photo(size = :medium)
-    if photos.attached?
+    first_photo = photos.joins(:images_attachments).first
+    if first_photo&.images&.attached?
       size_options = { medium: [400, 300], thumb: [100, 100] }
-      photos.first.variant(resize_to_limit: size_options[size] || size_options[:medium]).processed
+      first_photo.images.first.variant(resize_to_limit: size_options[size] || size_options[:medium]).processed
     else
       "blank.jpg"
     end
@@ -60,6 +64,9 @@ class Bouncehouse < ApplicationRecord
       "tip",
       "updated_at",
       "user_id"
-    ]
+
+
+
+end  end    ]    ]
   end
 end
