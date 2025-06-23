77b9151877b44595b0cec1eb6beb7ce926867e88
Lawrence <lawrencemurry@yahoo.com>
2025-06-23 04:23:00 -0500

    Use S3 for Active Storage in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,9 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  # config.active_storage.service = :production
+  config.active_storage.service = :amazon
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
