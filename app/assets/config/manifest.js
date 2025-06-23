033cb6f8bc6b3d583ac90302fb520f9c18299fa6
Lawrence <lawrencemurry@yahoo.com>
2025-06-23 02:07:24 -0500

    Fix Redis SSL verification for Heroku

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,5 @@
 //= link_tree ../images
 //= link application.css
-<<<<<<< HEAD
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
-=======
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
->>>>>>> 16de8cb7 (Updated App to Ruby 3.2.3 and Rails 7)
