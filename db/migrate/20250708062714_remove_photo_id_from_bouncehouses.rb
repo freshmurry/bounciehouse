d157745fcc5d52726c253af8b441b95e8725a7a8
Lawrence <lawrencemurry@yahoo.com>
2025-07-25 17:42:00 -0500

    Remove migration file

--- db/migrate/20250708062714_remove_photo_id_from_bouncehouses.rb
+++
@@ -1,5 +0,0 @@
-class RemovePhotoIdFromBouncehouses < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :bouncehouses, :photo_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_08_062714) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_08_000000) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_08_062714) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "photo_id"
     t.index ["user_id"], name: "index_bouncehouses_on_user_id"
   end
 
